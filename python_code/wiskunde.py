2a963ea5681d9a355645d0eb7abb122d6d55f515
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-04-08 15:45:35 +0200

    Create wiskunde.py

---
+++ python_code/wiskunde.py
@@ -0,0 +1,16 @@
+class Wiskunde:
+    def __init__(self):
+        self.test = None
+        self.versterkingsfactor = None
+        self.temp = None
+        
+    def volt2_temp(self,Max_temp,Min_temp,Max_volt,Min_volt,huidig_voltage):
+        self.versterkingsfactor = (Max_temp - Min_temp) / (Max_volt - Min_volt)
+        self.temp = (huidig_voltage * self.versterkingsfactor) + Min_temp
+        self.temp = round(self.temp,2)
+        
+if __name__ == "__main__":
+    wiskunde = Wiskunde()
+    wiskunde.volt2_temp(30,10,3.3,0,0.75)
+    print(wiskunde.temp)
+    

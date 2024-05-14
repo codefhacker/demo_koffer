f50a328931a041d305dd19feb0111940a575a7f9
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-05-14 12:30:21 +0200

    Update hardware.py

--- python_code/hardware.py
+++ python_code/hardware.py
@@ -31,12 +31,9 @@ class Hardware:
         self.schakelaar_1 = None
         self.schakelaar_2 = None
         self.schakelaar_3 = None
-        self.schakelaar_4 = None
-        self.schakelaar_5 = None
-        self.schakelaar_6 = None
-        self.schakelaar_7 = None
-        self.schakelaar_8 = None
-        self.schakelaar_9 = None
+      
+        self.led_werkend = None
+        self.led_storing = None
         
         self.servo = None
         self.servo_status = None
@@ -55,18 +52,38 @@ class Hardware:
         self.adc_3_channel_2 = AnalogIn(adc_3, ADS.P2)
         self.adc_4_channel_3 = AnalogIn(adc_4, ADS.P3)
         
-    def setup_neopixel(self,num_leds):
-        self.led_strip = neopixel.NeoPixel(board.D18, num_leds, brightness=0.1)
-    
+    def setup_neopixel(self,num_leds,lichtsterkte):
+        self.led_strip = neopixel.NeoPixel(board.D18, num_leds, brightness=int(lichtsterkte))
+        self.led_strip.fill((0, 0, 0))
+        
     def setup_leds(self,pin_hoog_urgent,pin_laag_urgent,pin_in_bedrijf):
         self.led_hoog_urgent= LED(pin_hoog_urgent)
         self.led_laag_urgent= LED(pin_laag_urgent)
         self.led_in_bedrijf = LED(pin_in_bedrijf)
+    
+    def setup_status_leds(self,pin_1):
+        self.led_werkend = LED(pin_1)
+        
+        
+    
+    def setup_fan(self,fan_pin):
+        self.fan = LED(fan_pin) # fan is natuurlijk geen led maar LED functie is aansturing van gpio pin hoog of laag daarom led
         
     def control_leds(self,hoog,laag, in_bedrijf):
         self.led_hoog_urgent.value = hoog
         self.led_laag_urgent.value = laag
         self.led_in_bedrijf.value = in_bedrijf
+    
+    def control_status_leds(self,state):
+        self.led_werkend.value = state
+        
+        
+        
+    
+    def control_fan(self,state):
+        self.fan.value = state
+        
+        
         
         
         
@@ -76,17 +93,13 @@ class Hardware:
         self.schakelaar_1 = Button(schakelaars[1], pull_up=False)
         self.schakelaar_2 = Button(schakelaars[2], pull_up=False)
         self.schakelaar_3 = Button(schakelaars[3], pull_up=False)
-        self.schakelaar_4 = Button(schakelaars[4], pull_up=False)
-        self.schakelaar_5 = Button(schakelaars[5], pull_up=False)
-        self.schakelaar_6 = Button(schakelaars[6], pull_up=False)
-        self.schakelaar_7 = Button(schakelaars[7], pull_up=False)
-        self.schakelaar_8 = Button(schakelaars[8], pull_up=False)
-        self.schakelaar_9 = Button(schakelaars[9], pull_up=False)
+        
+      
     
     def setup_servo(self,servo_pin):
         factory = PiGPIOFactory()
         self.servo = AngularServo(12, min_angle=-90, max_angle=90, pin_factory=factory) #hardware servo control 
-        
+        self.servo.angle = -90
         
     def move_servo(self, ticker):
         min_input = 0
@@ -95,12 +108,12 @@ class Hardware:
         max_output = 90
 
         geschaalde_waarde = (ticker - min_input) / (max_input - min_input) * (max_output - min_output) + min_output #calculate ticker 
-        
         self.servo.angle = geschaalde_waarde
-        print(geschaalde_waarde)
+        
         
     
     def led_strip_control(self, vraag, status_verwarmen):
+        print(vraag)
         if vraag:
             if status_verwarmen == 1:
                 red = int(vraag) + 155
@@ -108,10 +121,11 @@ class Hardware:
                 green = 100 - int(vraag)
                 self.led_strip.fill((red, green, blue))
             else:
-                blue = int(vraag) + 155
-                red = 100 - int(vraag)
-                green = 100 - int(vraag)
-                self.led_strip.fill((red, green, blue))
+                #blue = int(vraag) + 155
+                #red = 100 - int(vraag)
+                green = 255 - int(vraag) * 255 // 100
+                print("groen: " , green)
+                self.led_strip.fill((0, green, 255))
                 
         else:
                 # Als er geen vraag is, zet de LED-strip uit
@@ -134,4 +148,13 @@ if __name__ == "__main__":
             ticker = 0
         print(ticker)
         sleep(0.1)
+    
+        
+
+            
+    
+    
+    
+        
+        
     

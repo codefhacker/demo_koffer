623a9cd907a9284f357a3bfb5451b68d346784d0
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-05-29 11:51:36 +0200

    Update wiskunde.py

--- python_code/wiskunde.py
+++ python_code/wiskunde.py
@@ -1,45 +1,16 @@
-import time
-
 class Wiskunde:
     def __init__(self):
         self.test = None
         self.versterkingsfactor = None
         self.temp = None
-        self.procent_regelaar = None
         
     def volt2_temp(self,Max_temp,Min_temp,Max_volt,Min_volt,huidig_voltage):
         self.versterkingsfactor = (Max_temp - Min_temp) / (Max_volt - Min_volt)
         self.temp = (huidig_voltage * self.versterkingsfactor) + Min_temp
         self.temp = round(self.temp,1)
         
-    def offset2_procent(self,Max_procent,Min_procent,Max_offset,Min_offset,huidig_offset):
-        versterkingsfactor = (Max_procent - Min_procent) / (Max_offset - Min_offset)
-        self.procent_regelaar = (huidig_offset * versterkingsfactor) + Min_offset
-        
-        
-        self.procent_regelaar = self.procent_regelaar
-        self.procent_regelaar = round(self.procent_regelaar,1)
-        
-        
-            
-            
-ticker = 0
-        
 if __name__ == "__main__":
-    for i in range(100):
-        
-        wiskunde = Wiskunde()
-        ticker += 0.5
-        wiskunde.offset2_procent(-100,100,20,0,ticker)
-        print(wiskunde.procent_regelaar)
-        time.sleep(0.2)
-        wiskunde.offset2_procent(-100,100,20,0,ticker)
-        print(wiskunde.procent_regelaar)
-            
-        if ticker >=10:
-            ticker = 0
-                
-     
-        
-    
+    wiskunde = Wiskunde()
+    wiskunde.volt2_temp(30,10,3.3,0,0.75)
+    print(wiskunde.temp)
     

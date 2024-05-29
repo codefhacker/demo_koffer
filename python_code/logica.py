797b244470a36a105b6fb8ea21b453ced7619a18
fabian boshoven <65905014+codefhacker@users.noreply.github.com>
2024-05-29 11:50:38 +0200

    Update logica.py

--- python_code/logica.py
+++ python_code/logica.py
@@ -1,18 +1,3 @@
-"""
-logica functies voor de demo koffer 
-
-berekenstatus is voor het controleren van de status van de demo koffer
-
-Gemaakt door Fabian Boshoven
-
-Versie 0.9
-
-
-"""
-
-
-
-
 class Logica:
     def __init__(self):
         self.status_hoog_urgente_storing = None
@@ -21,8 +6,9 @@ class Logica:
         self.status_fan = None
         self.status_klep = None
         self.status_led_strip = None
-        self.list_heater = None  # Toevoeging: initialiseer list_heater
+        self.list_heater = None 
         self.status_verwarmen = None
+        self.lcd_string = None
         
     def bereken_status(self, tijd_status, fan_status, klep_status, filter_status):
         if tijd_status == 1 and fan_status == 0 and klep_status == 0:
@@ -33,10 +19,12 @@ class Logica:
             self.status_laag_urgente_storing = 0
             if filter_status == 1:
                 self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: filter"
             else:
                 self.status_laag_urgente_storing = 0
+                self.lcd_string = ""
                 
-        elif tijd_status == 1 and (fan_status == 1 or klep_status == 1):
+        elif tijd_status == 1 and fan_status == 1 and klep_status == 0:
             self.status_in_bedrijf = 0
             self.status_fan = 0
             self.status_klep = 0
@@ -44,10 +32,12 @@ class Logica:
             self.status_laag_urgente_storing = 0
             if filter_status == 1:
                 self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: fan,filter"
             else:
                 self.status_laag_urgente_storing = 0
-            
-        elif fan_status == 1 or klep_status == 1:
+                self.lcd_string = "storing: fan"
+                
+        elif tijd_status == 1 and klep_status == 1 and fan_status == 0:
             self.status_in_bedrijf = 0
             self.status_fan = 0
             self.status_klep = 0
@@ -55,21 +45,83 @@ class Logica:
             self.status_laag_urgente_storing = 0
             if filter_status == 1:
                 self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: klep,filter"
             else:
                 self.status_laag_urgente_storing = 0
+                self.lcd_string = "storing: klep"
+                
+        elif tijd_status == 1 and klep_status == 1 and fan_status == 1:
+            self.status_in_bedrijf = 0
+            self.status_fan = 0
+            self.status_klep = 0
+            self.status_hoog_urgente_storing = 1
+            self.status_laag_urgente_storing = 0
+            if filter_status == 1:
+                self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: alles"
+            else:
+                self.status_laag_urgente_storing = 0
+                self.lcd_string = "storing: klep,fan"
+                
+        elif fan_status == 1 and klep_status == 1:
+            self.status_in_bedrijf = 0
+            self.status_fan = 0
+            self.status_klep = 0
+            self.status_hoog_urgente_storing = 1
+            self.status_laag_urgente_storing = 0
+            if filter_status == 1:
+                self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: alles"
+            else:
+                self.status_laag_urgente_storing = 0
+                self.lcd_string = "storing: klep,fan"
+                
+        elif klep_status == 1 and fan_status == 0:
+            self.status_in_bedrijf = 0
+            self.status_fan = 0
+            self.status_klep = 0
+            self.status_hoog_urgente_storing = 1
+            self.status_laag_urgente_storing = 0
+            if filter_status == 1:
+                self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: klep,filter"
+            else:
+                self.status_laag_urgente_storing = 0
+                self.lcd_string = "storing: klep"
+        
+        elif fan_status == 1 and klep_status == 0:
+            self.status_in_bedrijf = 0
+            self.status_fan = 0
+            self.status_klep = 0
+            self.status_hoog_urgente_storing = 1
+            self.status_laag_urgente_storing = 0
+            if filter_status == 1:
+                self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: fan,filter"
+            else:
+                self.status_laag_urgente_storing = 0
+                self.lcd_string = "storing: fan"
+        
+        
+        
+                
             
         else:
             self.status_in_bedrijf = 0
             self.status_fan = 0
             self.status_klep = 0
             self.status_hoog_urgente_storing = 0
+            
             if filter_status == 1:
                 self.status_laag_urgente_storing = 1
+                self.lcd_string = "storing: filter"
+                
             else:
                 self.status_laag_urgente_storing = 0
+                self.lcd_string = ""
     
     # Toevoeging: functie om de heater output lijst te berekenen
-    def bereken_vermogen(self, Kr, setpoint, initial_temperature):
+    def bereken_vermogen(self, Kr, setpoint, huidige_temperatuur):
         # Functie om proportionele controle uit te voeren en de verwarmingsoutput te retourneren
         def proportional_control(Kr, setpoint, current_temperature):
             error = setpoint - current_temperature
@@ -81,12 +133,13 @@ class Logica:
             return output_percentage
     
         heater_output = 100
+        error_2 = 100 - ((setpoint - huidige_temperatuur) * 5)
         self.list_heater = []
-    
+        
         while heater_output > 1:
-            heater_output = proportional_control(Kr, setpoint, initial_temperature)
-            output_2 = 100 - heater_output
-            initial_temperature += heater_output * 0.01  # Simulate system response
+            heater_output = proportional_control(Kr, setpoint, huidige_temperatuur)
+            output_2 = (100 - heater_output) - error_2
+            huidige_temperatuur += heater_output * 0.01  # Simulate system response
             self.list_heater.append(int(output_2))
     
         return self.list_heater
@@ -102,7 +155,4 @@ if __name__ == "__main__":
     print("Status Laag Urgente Storing:", logica_instance.status_laag_urgente_storing)
     
     print(logica_instance.bereken_vermogen(22,19,15))
-
-
-
 
